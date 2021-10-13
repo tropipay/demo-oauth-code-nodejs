a87d6349040ad719111a353c4d07f26a0169f4ba
Antonio Membrides Espinosa <tonykssa@gmail.com>
2021-10-13 19:29:50 -0400

    upload demo

---
+++ index.js
@@ -0,0 +1,77 @@
+const app = require('express')();
+const axios = require('axios');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//......................................................
+const oauth_authorize = 'http://localhost:3001/api/v2/access/authorize';
+const oauth_token = 'http://localhost:3001/api/v2/access/token';
+
+const client_id = "1b125cefa4e6aa5fc044a06190953eac";
+const client_secret = "6fdd1a8b146b22be1057d38f2b672e7d";
+
+const redirect_uri = "http://localhost:5000/oauth/response";
+const scope = "ALLOW_GET_BALANCE";
+const state = "abcd-1234";
+const code_verifier = "1234-abcd-1234";
+const code_challenge = "N2_wPQ7X9iP5bKXcw05rqHw1S7OwFuU4Nqi6ccr_LEs";
+const code_challenge_method = "S256";
+
+//......................................................
+
+app.get('/', (req, res, next) => {
+	res.end('<a href="/user/balance"> User Balance </a>');
+});
+
+app.get('/user/balance', (req, res, next) => {
+	//... OAUTH STEP 1
+	const param = "?response_type=code" +
+				"&client_id=" + client_id +
+				"&client_secret=" + client_secret + 
+				"&redirect_uri=" + redirect_uri + 
+				"&code_challenge=" + code_challenge + 
+				"&code_challenge_method=" + code_challenge_method + 
+				"&state=" + state + 
+				"&scope=" + scope;
+	res.redirect(oauth_authorize + param);
+});
+
+app.get('/oauth/response', async (req, res, next) => {
+	let access_token = "";
+	try{
+		//... OAUTH STEP 2
+		if(req.query['state'] !== state){
+			console.log('NOT secure, the state value not match');
+		}
+		const param = {
+			grant_type: "authorization_code",
+			code: req.query['code'],
+			client_id,
+			client_secret,
+			redirect_uri,
+			code_verifier,
+			scope
+		};
+		const token = await axios.post(oauth_token, param);
+		access_token = token.data.access_token;
+	}
+	catch(error){
+		res.end('OAUTH: Not authorize');
+	}
+	try {
+		//... Get service
+		const response = await axios({
+			headers: { 'Authorization': 'Bearer '+ access_token },
+			url: "http://localhost:3000/api/users/balance"
+		});
+		res.end(`<h1> Balance: ${response.data.balance} </h1>`);
+	}
+	catch(error){
+		res.end('Service: Not authorize');
+	}
+});
+
+//......................................................
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`>>> http://localhost:${PORT}`))
